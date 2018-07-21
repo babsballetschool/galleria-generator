1b1ca5486582b6b115301d3e5a8ff7a777206712
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-07-21 08:32:15 +0200

    Choose title on enter

--- src/GalleriaGenerator/Galleria.elm
+++ src/GalleriaGenerator/Galleria.elm
@@ -2,6 +2,8 @@ module GalleriaGenerator.Galleria exposing (Gallery, Message, update, view)
 
 import Html
 import Html.Attributes as Attribute
+import Html.Events as Event
+import Json.Decode as Json
 
 
 -- Model
@@ -26,18 +28,26 @@ type alias Photo =
 
 
 type Message
-    = ChangeTitle
+    = DoNothing
+    | ChangeTitle
     | UpdateTitle String
+    | ChooseTitle
 
 
 update : Message -> Gallery -> ( Gallery, Cmd msg )
 update message gallery =
     case message of
+        DoNothing ->
+            ( gallery, Cmd.none )
+
         ChangeTitle ->
             ( { gallery | changingTitle = True }, Cmd.none )
 
         UpdateTitle newTitle ->
-            ( { gallery | changingTitle = False, title = newTitle }, Cmd.none )
+            ( { gallery | title = newTitle }, Cmd.none )
+
+        ChooseTitle ->
+            ( { gallery | changingTitle = False }, Cmd.none )
 
 
 
@@ -47,5 +57,44 @@ update message gallery =
 view : Gallery -> Html.Html Message
 view gallery =
     Html.div [ Attribute.class "gallery" ]
-        [ Html.h1 [ Attribute.class "title" ] [ Html.text gallery.title ]
+        [ (title gallery)
         ]
+
+
+title : Gallery -> Html.Html Message
+title gallery =
+    if not gallery.changingTitle then
+        Html.h1
+            [ Attribute.class "title"
+            , Event.onClick ChangeTitle
+            ]
+            [ Html.text gallery.title ]
+    else
+        Html.span
+            [ Attribute.classList
+                [ ( "title", True )
+                , ( "changing", True )
+                ]
+            ]
+            [ Html.input
+                [ Attribute.type_ "input"
+                , Attribute.defaultValue gallery.title
+                , Event.onInput (\newTitle -> UpdateTitle newTitle)
+                , Event.onBlur ChooseTitle
+                , onKeyDown (whenEnter ChooseTitle)
+                ]
+                []
+            ]
+
+
+onKeyDown : (Int -> msg) -> Html.Attribute msg
+onKeyDown tagger =
+    Event.on "keydown" (Json.map tagger Event.keyCode)
+
+
+whenEnter : Message -> Int -> Message
+whenEnter message index =
+    if index == 13 then
+        message
+    else
+        DoNothing
